dee29585ff04360cfe474cfc5992d520e709f588
nikithapk <nikithapoolanda@gmail.com>
2020-02-16 17:45:47 +0530

    initial commit

--- dist/index.php
+++ dist/index.php
@@ -1,14 +1,24 @@
-<?php
+	<?php
     session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+     // The request is using the POST method
 
+ $password = $_POST['password'];
+ 
+ 
+ if ($password == "iamadmin"){
+ $_SESSION["goodiekiosk"] = True;
+ }else{
+ echo "Invalid password. ";
+}}
     if (!isset($_SESSION['goodiekiosk'])) {
-       echo "You are not authrosied, Please login";
+       echo "You are not authrosied, Please login. ";
 	   echo '
 	   <html>
    
    <head>
       <title>Login Page</title>
-      
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <style type = "text/css">
          body {
             font-family:Arial, Helvetica, sans-serif;
@@ -60,17 +70,7 @@ exit;
 	   
     }
 	
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-     // The request is using the POST method
-
- $password = $_POST['password'];
- 
- 
- if ($password == "iamadmin")
- $_SESSION["goodiekiosk"] = True;
- }else{
- echo "Invalid password"
-}
+	
 ?>
 <!DOCTYPE html>
 <html lang="en" >
@@ -79,13 +79,25 @@ exit;
   <title>CodePen - Bare QR reader demo</title>
   <link rel="stylesheet" href="./style.css">
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<style>
+body {
+    text-align: center;
+}
+form {
+    display: inline-block;
+}
+</style>
 </head>
 <body>
 <!-- partial:index.partial.html -->
 <form method="POST" action="validate_qr.php">
+<h2>SSE Expo 2020 </h2>
+
+<h3>Claim Goodie Tool </h3>
+<br><br><br>
 <input name ="qr_text" type=text size=16 placeholder="Tracking Code" class=qrcode-text><label class=qrcode-text-btn><input type=file accept="image/*" capture=environment onchange="openQRCamera(this);" tabindex=-1></label> 
 <input type="hidden" name="qr_key" value="5b50207e7779ec6c9e4fdd91064b350d">
+<br><br><br>
 <input type="submit" value="Go" >
 </form>
 <!-- partial -->
