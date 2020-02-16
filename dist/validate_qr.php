99f1faa4c8b4db6ec3211828e55361ebe72f9687
nikithapk <nikithapoolanda@gmail.com>
2020-02-16 17:18:54 +0530

    first commit

--- dist/validate_qr.php
+++ dist/validate_qr.php
@@ -1,11 +1,12 @@
 <?php
-$qrkey = $_POST['qr_key'];
-$associd = $_POST['qr_text'];
+echo $qrkey = $_GET['qr_key'];
+echo $associd = $_GET['qr_text'];
 
-if ($qr_key == "5b50207e7779ec6c9e4fdd91064b350d")
-{
-	echo $associd;
+
+// if ($qr_key == "5b50207e7779ec6c9e4fdd91064b350d")
+// {
+	// echo $associd;
 	
-}else
-echo "Invalid API Key");
+// }else
+// echo "Invalid API Key");
 ?>
