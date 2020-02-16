0c19714484a7356ac41892f1f26e40621025f3ce
nikithapk <nikithapoolanda@gmail.com>
2020-02-16 17:34:10 +0530

    first commit

---
+++ dist/index.php
@@ -0,0 +1,95 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['goodiekiosk'])) {
+       echo "You are not authrosied, Please login";
+	   echo '
+	   <html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                 
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
+
+   </body>
+</html>
+';
+
+exit;
+	   
+	   
+	   
+	   
+	   
+    }
+	
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+     // The request is using the POST method
+
+ $password = $_POST['password'];
+ 
+ 
+ if ($password == "iamadmin")
+ $_SESSION["goodiekiosk"] = True;
+ }else{
+ echo "Invalid password"
+}
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Bare QR reader demo</title>
+  <link rel="stylesheet" href="./style.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<form method="POST" action="validate_qr.php">
+<input name ="qr_text" type=text size=16 placeholder="Tracking Code" class=qrcode-text><label class=qrcode-text-btn><input type=file accept="image/*" capture=environment onchange="openQRCamera(this);" tabindex=-1></label> 
+<input type="hidden" name="qr_key" value="5b50207e7779ec6c9e4fdd91064b350d">
+<input type="submit" value="Go" >
+</form>
+<!-- partial -->
+  <script src='https://dmla.github.io/jsqrcode/src/qr_packed.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
--- dist/validate_qr.php
+++ dist/validate_qr.php
@@ -1,12 +1,13 @@
 <?php
-echo $qrkey = $_GET['qr_key'];
-echo $associd = $_GET['qr_text'];
+ $qrkey = $_POST['qr_key'];
+ $associd = $_POST['qr_text'];
 
 
-// if ($qr_key == "5b50207e7779ec6c9e4fdd91064b350d")
-// {
-	// echo $associd;
-	
-// }else
-// echo "Invalid API Key");
+if ($qr_key == "5b50207e7779ec6c9e4fdd91064b350d")
+{
+	echo $associd;
+	sleep(5);
+	header('Location: http://cyberboy.in/Space/qrreader/dist/');
+}else
+echo "Invalid API Key");
 ?>
