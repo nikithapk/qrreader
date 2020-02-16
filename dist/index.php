328e60fb96d85c80d400d00a388188fcb486de04
nikithapk <nikithapoolanda@gmail.com>
2020-02-16 18:36:34 +0530

    V1.1

--- dist/index.php
+++ dist/index.php
@@ -76,7 +76,7 @@ exit;
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Bare QR reader demo</title>
+  <title>SSE Expo Goodie Tool</title>
   <link rel="stylesheet" href="./style.css">
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <style>
--- dist/validate_qr.php
+++ dist/validate_qr.php
@@ -1,3 +1,14 @@
+
+<html>
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+
+</head>
+<body>
+
+
+</body>
+
 <?php
  $qrkey = $_POST['qr_key'];
  $associd = $_POST['qr_text'];
@@ -5,9 +16,45 @@
 
 if ($qrkey == "5b50207e7779ec6c9e4fdd91064b350d")
 {
-	echo $associd;
-	sleep(5);
-	header('Location: http://cyberboy.in/Space/qrreader/dist/');
+	echo '<br><br><br><br><br><div align="center" id="dvCountDown" style = "display:none">
+You will be redirected after <span id = "lblCount"></span>&nbsp;seconds.
+
+
+<br>
+<br>
+
+<img src="https://www.kindpng.com/picc/m/164-1645081_computer-icons-check-mark-presentation-symbol-gif-animation.png" alt="Smiley face" height="100" width="100">
+
+<br>
+
+<br>
+'.$associd.'
+<br>
+</div>
+';
+	
 }else
 echo "Invalid API Key";
+
 ?>
+<script type="text/javascript">
+window.onload=DelayRedirect;
+function DelayRedirect() {
+	// window.alert("sometext");
+    var seconds = 5;
+    var dvCountDown = document.getElementById("dvCountDown");
+    var lblCount = document.getElementById("lblCount");
+    dvCountDown.style.display = "block";
+    lblCount.innerHTML = seconds;
+    setInterval(function () {
+		
+        seconds--;
+        lblCount.innerHTML = seconds;
+        if (seconds == 0) {
+            dvCountDown.style.display = "none";
+            window.location = "./";
+        }
+    }, 1000);
+}
+</script>
+</html>
