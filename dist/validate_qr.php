776ebd49446387d67474a19777805ffff240fea9
nikithapk <nikithapoolanda@gmail.com>
2020-02-16 17:51:25 +0530

    INIT

--- dist/validate_qr.php
+++ dist/validate_qr.php
@@ -3,11 +3,11 @@
  $associd = $_POST['qr_text'];
 
 
-if ($qr_key == "5b50207e7779ec6c9e4fdd91064b350d")
+if ($qrkey == "5b50207e7779ec6c9e4fdd91064b350d")
 {
 	echo $associd;
 	sleep(5);
 	header('Location: http://cyberboy.in/Space/qrreader/dist/');
 }else
-echo "Invalid API Key");
+echo "Invalid API Key";
 ?>
